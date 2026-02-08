24f054ed49786b834d6a9e655099477237168b7a
mohammad <mmdcj8499@gmail.com>
2026-02-08 15:25:37 +0330

    feat: add openTelemetry configuration

--- src/ClojureHighlighter/Program.cs
+++ src/ClojureHighlighter/Program.cs
@@ -1,15 +1,42 @@
 using ClojureHighlighter.Application;
 using ClojureHighlighter.Application.FactoryMethods;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IClojureParserServiceFactory, ClojureParserServiceFactory>();
 builder.Services.AddScoped<IClojureTokenizerServiceFactory, ClojureTokenizerServiceFactory>();
 builder.Services.AddScoped<ISyntaxHighlighter, SyntaxHighlighterService>();
-
 builder.Services.AddControllers();
 
-var app = builder.Build();
 
+/*
+ * by default the telemetry data are sent via Grpc protocol
+ */
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracing => tracing
+        .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+        .AddAspNetCoreInstrumentation()
+        .AddOtlpExporter())
+    .WithMetrics(metrics => metrics
+        .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+        .AddAspNetCoreInstrumentation()
+        .AddOtlpExporter());
+
+
+builder.Logging.AddOpenTelemetry(logging =>
+{
+    logging.IncludeFormattedMessage = true;
+    logging.IncludeScopes = true;
+    logging.SetResourceBuilder(ResourceBuilder.CreateDefault()
+        .AddService(serviceName: builder.Environment.ApplicationName));
+    logging.AddOtlpExporter();
+});
+
+
+var app = builder.Build();
 
 
 app.MapControllers();
