d7272c49afa0e99f41e3e018dbac8bd56872b30a
mohammad <mmdcj8499@gmail.com>
2026-02-08 21:14:51 +0330

    feat: add an identifier node with its corresponding roles as an enum type

---
+++ src/FortranHighlighter/Domain/ASTNode/IdentifierNode.cs
@@ -0,0 +1,26 @@
+namespace FortranHighlighter.Domain.ASTNode;
+using FortranHighlighter.Domain.Token;
+
+public class IdentifierNode
+{
+    public Token Token { get; set; }
+    public IdentifierRole Role { get; set; }
+    
+    public IdentifierNode(Token token, IdentifierRole role = IdentifierRole.Unknown)
+    {
+        Token = token;
+        Role = role;
+    }
+}
+
+public enum IdentifierRole
+{
+    Unknown,
+    ProgramName,
+    SubroutineName,
+    FunctionName,
+    FunctionCall,
+    VariableName,
+    Parameter,
+    LoopVariable
+}
